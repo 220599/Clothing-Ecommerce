9f6b1341a53910f19a0443a012dc8004b11c3df6
Shubham <shubhamsalunke543@gmail.com>
2024-06-23 21:58:40 -0400

    Initial commit

---
+++ ecommerce/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import BarChart from './Components/Navbar/Chart';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
